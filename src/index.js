b559cc543ba8060c3813d0851e964f1f74c1c7cf
AkaraChen <85140972+AkaraChen@users.noreply.github.com>
2021-09-02 15:52:53 +0800

    first init

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import Vue from '../node_modules/vue/dist/vue.js'
+
+new Vue({
+    el:'#app',
+    data:{
+        date:'2021年9月1日',
+        color:'黑',
+        circle:'c++',
+        game:'Visual Studio',
+        price:'2w人民币'
+    }
+})
